21981aeb95e7ad08afa02a650d4b4ef23ef69213
gautamrai-28 <gautamrai1321@gmail.com>
2026-07-17 20:53:59 +0530

    Time: 0 ms (100%), Space: 118 MB (82.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,32 +8,64 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ /*there would be total 3 steps
+    1. Find the middle of list
+    2. reverse first half or second half of the list
+    3. compare both list if not equal return false else true
+*/
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int>ans;
+    //step 2 : reverse second half of the list
+    ListNode* reverseList(ListNode* head) {
 
-        ListNode* temp = head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
 
-        while(temp != NULL) {
-            ans.push_back(temp->val);
-            temp = temp->next;
+        while(curr!=NULL) {
+            ListNode* forwad = curr->next;
+
+            curr->next = prev;
+
+            prev = curr;
+            curr = forwad;
         }
         
-        int flag = 0;
-        int size = ans.size();
+        head = prev;
+        return head;
+    }
 
-        int i = 0, j = size-1;
+    bool isPalindrome(ListNode* head) {
 
-        while(i<j) {
-            if(ans[i] != ans[j]) 
-              flag = 1;
-            
-            i++;
-            j--;
+        // if(head == NULL || head->next == NULL)
+        //    return true;
+
+        //step 1: find midde of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if(flag == 1) return false;
+
+        //step2
+        ListNode* head2 = reverseList(slow);
+
+        ListNode* curr1 = head;
+        ListNode* curr2 = head2;
+
+        //step 3: comapre both lists
+        while(curr1 != NULL && curr2 != NULL) {
+
+            if(curr1->val != curr2->val) 
+              return false;
+            
+            curr1 = curr1->next;
+            curr2 = curr2->next;
+        }
 
         return true;
+
     }
 };
