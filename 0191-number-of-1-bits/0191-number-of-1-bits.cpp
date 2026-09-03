57a8a21eab221593f618d8497a071d714444255d
gautamrai-28 <gautamrai1321@gmail.com>
2026-09-03 20:31:54 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.15%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int setBits = 0;
+
+        while(n!=0) {
+            if(n%2 == 1) setBits++;
+
+            n/=2;
+        } 
+        return setBits;
+    }
+};
